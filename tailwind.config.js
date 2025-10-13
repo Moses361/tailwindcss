8a25e8a0dff4404fb38fa6d72a13ed392be64b43
Moses361 <odhismoses20@gmail.com>
2025-10-13 19:47:53 +0300

    Add full Tailwind CSS project files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./**/*.html", "./**/*.js"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "480px",
+      },
+      spacing: {
+        big: "48rem",
+      },
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
